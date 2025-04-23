43bbe633decf6f911fc2699faca13a561737cf8a
Siddharth142004 <siddharthjayachandran2004@gmail.com>
2025-04-23 10:46:32 +0530

    azure pipeline example

---
+++ src/main/java/com/prog10/mavenazure/App.java
@@ -0,0 +1,13 @@
+package com.prog10.mavenazure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
